de42c3b691f2291934dbdf14f687000459a6d8e7
Wmugacha <wilfredmugacha@gmail.com>
2025-07-10 19:02:38 +0800

    cache property list

--- properties/views.py
+++ properties/views.py
@@ -2,9 +2,10 @@ from django.views.decorators.cache import cache_page
 from django.http import JsonResponse
 from django.core import serializers
 from .models import Property
+from .serializers import PropertySerializer
 
 @cache_page(60 * 15)
 def property_list(request):
     properties = Property.objects.all()
-    data = serializers.serialize('json', properties)
-    return JsonResponse(data, safe=False)
+    serializer = PropertySerializer(properties, many=True)
+    return JsonResponse({"data": serializer.data})

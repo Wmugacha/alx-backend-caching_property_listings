036302a1555f3f310699998d129f44f0d6b6dbde
Wmugacha <wilfredmugacha@gmail.com>
2025-07-10 11:29:58 +0800

    project setup in docker containers

--- properties/models.py
+++ properties/models.py
@@ -5,7 +5,7 @@ User = get_user_model()
 
 class Property(models.Model):
     title = models.CharField(max_length=200)
-    description = models.TextField
+    description = models.TextField(default="No description provided")
     price = models.DecimalField(max_digits=10, decimal_places=2)
     location = models.CharField(max_length=100)
     created_at = models.DateTimeField(auto_now_add=True)
